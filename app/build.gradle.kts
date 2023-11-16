41d83ef8c837f0d6af340e5ae3586297db8c323c
arty314 <yu21iarty@gmail.com>
2023-11-16 22:25:11 +0900

    connect_firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+
+    // Google 서비스 Gradle 플러그인 추가
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -36,4 +39,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.2" apply false
+
+    // Google 서비스 Gradle 플러그인
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
