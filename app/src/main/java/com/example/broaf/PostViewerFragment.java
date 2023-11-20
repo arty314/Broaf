18863390a4675ff4d996ed53f6ea9122a69145c2
SEjiji <amaranth950927@gmail.com>
2023-11-20 12:40:52 +0900

    뷰어 닫기를 만들다가 뭔가 안돼서 주석만 걸어둠

--- app/src/main/java/com/example/broaf/PostViewerFragment.java
+++ app/src/main/java/com/example/broaf/PostViewerFragment.java
@@ -7,59 +7,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 //그냥 빈 frag 만드니까 이렇게 떴음. 알아서 수정해주세요
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PostViewerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PostViewerFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public PostViewerFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PostViewerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PostViewerFragment newInstance(String param1, String param2) {
-        PostViewerFragment fragment = new PostViewerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+//    Button btn_close_viewer;    //뷰어 닫기 버튼
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post_viewer, container, false);
+        View view = inflater.inflate(R.layout.fragment_post_viewer, container, false);
+
+
+/*근데 뭔가 안 돼서 일단 주석 걸어둠
+        //post viewer frag 닫기
+        Button btn_close_viewer=(Button) getView().findViewById(R.id.btn_close_viewer);
+        btn_close_viewer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //현재 Frag 종료
+                getFragmentManager().popBackStack();
+            }
+        });
+        //여기까지 post viewer frag 닫기
+*/
+
+        return view;
     }
 }
