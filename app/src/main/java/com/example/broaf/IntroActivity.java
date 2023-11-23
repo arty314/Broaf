7ba328c88df18d30d7015fcce41ded71426e5fa4
SEjiji <amaranth950927@gmail.com>
2023-11-23 17:49:50 +0900

    key hash

--- app/src/main/java/com/example/broaf/IntroActivity.java
+++ app/src/main/java/com/example/broaf/IntroActivity.java
@@ -2,10 +2,18 @@ package com.example.broaf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class IntroActivity extends AppCompatActivity {
@@ -13,6 +21,23 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceStart) {
         super.onCreate(savedInstanceStart);
         setContentView(R.layout.first);
+
+
+        ///**
+        //키 해시 얻기
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("키해시는 :", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        //*키해시얻기:여기까지
 
         // 로그인 버튼을 눌렀을 때
         Button btnLogin = findViewById(R.id.btnLogin);
