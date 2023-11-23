8591d7fcece8a5ae2ba14ebed769e314ff0249b3
arty314 <yu21iarty@gmail.com>
2023-11-23 16:01:23 +0900

    help

--- app/src/main/java/com/example/broaf/LoginActivity.java
+++ app/src/main/java/com/example/broaf/LoginActivity.java
@@ -22,11 +22,10 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-//어플 시작 시 뜨는 로그인 화면
+
 public class LoginActivity extends AppCompatActivity {
 
-    //메인 acti넘어가게할 때 쓸 버튼. activity_login.xml의 동명의 버튼과 연결
-    private EditText getloginid, getloginpw;
+    private EditText getloginemail, getloginpw;
     private Button btnlogin;
     
     @Override
@@ -35,16 +34,16 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login);
 
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        getloginid=findViewById(R.id.getloginid);
+        getloginemail=findViewById(R.id.getloginemail);
         getloginpw=findViewById(R.id.getloginpw);
         btnlogin=findViewById(R.id.btnlogin);
 
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String id = getloginid.getText().toString();
+                String email = getloginemail.getText().toString();
                 String pw = getloginpw.getText().toString();
-                firebaseAuth.signInWithEmailAndPassword(id, pw)
+                firebaseAuth.signInWithEmailAndPassword(email, pw)
                         .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -59,23 +58,5 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-        //여기까지 메인 acti로 넘어가기.
-
-        ///**
-        //키 해시 얻기 (저희 팀 멤버들의 모든 개발환경 키 해시를 얻고 나면 삭제할 코드
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("키해시는 :", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        //*여기까지 키 해시 얻기*/
-
     }
 }
