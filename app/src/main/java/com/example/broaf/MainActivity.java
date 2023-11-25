0c06ef64124c3c5e58ed416395253abe8ae83367
DONGUKKIM248 <steam162636@yu.ac.kr>
2023-11-25 21:43:14 +0900

    검색 및 하단바 뒤로가기 문제점 추가 및 fragment_search.xml조금 추가했습니다.

--- app/src/main/java/com/example/broaf/MainActivity.java
+++ app/src/main/java/com/example/broaf/MainActivity.java
@@ -55,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
 
