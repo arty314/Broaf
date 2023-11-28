1ff733d67863522d46be8ddf3a5d263f43395d96
SEJIJI <amaranth@gmail.com>
2023-11-29 02:34:10 +0900

    centerbutton 형태 지정완료

--- app/src/main/java/com/example/broaf/PostViewerFragment.java
+++ app/src/main/java/com/example/broaf/PostViewerFragment.java
@@ -11,15 +11,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 public class PostViewerFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_post_viewer, container, false);
-
 
         return view;
     }
